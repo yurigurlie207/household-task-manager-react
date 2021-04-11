dc9a538551f1a385b525bb03db6e57bcbe2ef5c4
yurigurlie207 <adrienne.yao@gmail.com>
2021-04-11 12:57:24 -0400

    Hooked up redux store in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import manageTasks from './reducers/manageTasks';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(manageTasks);
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+document.getElementById('root')
 );
-
