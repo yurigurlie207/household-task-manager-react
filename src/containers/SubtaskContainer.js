04044108adf72091a4648dada49fc5f35759ccaf
yurigurlie207 <adrienne.yao@gmail.com>
2021-04-11 13:13:01 -0400

    Connected subtask container to redux store

--- src/containers/SubtaskContainer.js
+++ src/containers/SubtaskContainer.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SubtaskForm from '../components/subtasks/SubtaskForm'
+import Subtasks from '../components/subtasks/Subtasks'
+import { connect } from 'react-redux'
 
-const SubtaskContainer = () => {
-
-
-  return (
+class SubtaskContainer extends Component {
+  render() {
+    return (
       <div>
-          <h1>Subtasks</h1>
-         
-     
+        <SubtaskForm addSubtask={this.props.addSubtask}/>
+        <Subtasks
+          subtasks={this.props.subtasks}
+          deleteSubtask={this.props.deleteSubtask}
+        />
       </div>
-  )
+    );
+  }
 }
 
+const mapStateToProps = state => ({ subtasks: state.subtasks })
 
-export default SubtaskContainer
+const mapDispatchToProps = dispatch => ({
+  addSubtask: text => dispatch({type: 'ADD_SUBTASK', text}),
+  deleteSubtask: id => dispatch({type: 'DELETE_SUBTASK', id})
+})
 
+export default connect(mapStateToProps, mapDispatchToProps)(SubtaskContainer)
