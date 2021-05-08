fa34add77d0ee83d34f69de9ee687812371b8ba3
yurigurlie207 <adrienne.yao@gmail.com>
2021-05-08 12:47:23 -0400

    Put subtask fetch in with the user and usertask fetch page.

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Users from '../components/users/Users'
+import Subtasks from '../components/subtasks/Subtasks'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../actions/subtasks'
 import { fetchUsertasks } from '../actions/subtasks'
 import { deleteUsertasks } from '../actions/subtasks'
+import { fetchSubtasks } from '../actions/subtasks'
 
 class UserContainer extends Component {
 
@@ -29,13 +31,21 @@ class UserContainer extends Component {
 
     return (
       <div >
-        <button onClick={(event) => this.handleOnClick(event)}> Refresh Latest Usertasks </button>   
-        <p>Request Status: {status}</p>
-        <Users
-          users={this.props.users}
-          usertasks={this.props.usertasks}
-          delete={this.props.deleteUsertasks}
-        />
+        <button onClick={(event) => this.handleOnClick(event)}> Refresh Latest Task Assignments </button>   
+        <div>
+          <Subtasks
+            subtasks={this.props.subtasks}
+            users={this.props.users}
+          />
+        </div>
+        <div>
+            <p>Request Status: {status}</p>
+            <Users
+              users={this.props.users}
+              usertasks={this.props.usertasks}
+              delete={this.props.deleteUsertasks}
+            />
+        </div>  
       </div>
     );
   }
@@ -46,6 +56,7 @@ class UserContainer extends Component {
 const mapDispatchToProps = dispatch => ({
   fetchUsers: () => dispatch(fetchUsers()),
   fetchUsertasks: () => dispatch(fetchUsertasks()),
+  fetchSubtasks: () => dispatch(fetchSubtasks()),
   deleteUsertasks: (usertaskID) => dispatch(deleteUsertasks(usertaskID))
 })
 
@@ -53,6 +64,7 @@ const mapStateToProps = state => {
   return {
     users: state.users,
     usertasks: state.usertasks,
+    subtasks: state.subtasks,
     requesting: state.requesting
   }
 }
