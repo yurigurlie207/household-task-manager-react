96384d3613b8b43358d00da1c6827a7b3b503fde
yurigurlie207 <adrienne.yao@gmail.com>
2021-05-29 12:34:21 -0400

    Deleted uncessary links to parts of the app i will not build

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,8 @@ const NavBar = () => {
   return (
     <div className="navbar">
       <NavLink to="/"> Home </NavLink>
-      <NavLink to="/tasks"> Create Tasks </NavLink>
        <NavLink to="/subtasks"> Create Subtasks</NavLink>
        <NavLink to="/dashboard"> Dashboard </NavLink>
-       <NavLink to="/users"> Users </NavLink>
     </div>
   );
 };
