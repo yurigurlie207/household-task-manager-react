bf12ba8b4e2a230bd07d06e456fc0041ac0468cc
yurigurlie207 <adrienne.yao@gmail.com>
2021-07-12 16:36:21 -0400

    Also fixed an onclick button callback error causing immediate function execution in usertask component

--- src/components/users/Usertasks.js
+++ src/components/users/Usertasks.js
@@ -2,35 +2,36 @@ import React, { Component } from 'react';
 
 class Usertasks extends Component {
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-        usertasks: this.props.usertasks
-      }
-  };
+  //   this.state = {
+  //       usertasks: this.props.usertasks
+  //     }
+  // };
 
-  handleDeleteClick = (id) => {
-    this.props.delete(id);
-    this.props.remove(id)
+  handleDeleteClick = (event) => {
+    this.props.delete(event.target.id);
+    this.props.remove(event.target.id);
    }
 
   render() {
 
     let usertaskList = []
-    if (this.state.usertasks.data) {
-          const usertasks = this.state.usertasks.data
+    // debugger
+    if (this.props.usertasks.data) {
+          const usertasks = this.props.usertasks.data
    
           const userID = this.props.user.id 
           const associatedTasks = usertasks.filter(usertask => usertask.relationships.user.data.id === userID);
 
           usertaskList = associatedTasks.map( usertask => {
             
-            const subtask = this.state.usertasks.included.find(subtask => subtask.id === usertask.relationships.subtask.data.id)
+            const subtask = this.props.usertasks.included.find(subtask => subtask.id === usertask.relationships.subtask.data.id)
             return (
             // <li>{usertask.id}, {usertask.relationships.user.data.id}, {usertask.relationships.subtask.data.id}</li>
             <li key={usertask.id}>{subtask.attributes.title} :::
-            <button id={usertask.id} onClick={this.handleDeleteClick(usertask.id)}> Unassign </button>
+            <button id={usertask.id} onClick={this.handleDeleteClick}> Unassign </button>
             </li>
             )
           })
